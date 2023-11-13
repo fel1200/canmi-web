9e5a9b7dda15b63bd789dad9bb750d80576cf210
fel1200 <fel1200@hotmail.com>
2023-11-12 21:47:25 -0600

    Funcionalidad de descarga

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import { URL_API_ANSWERS, getToken } from "../../api";
 import { IndicatorSumarized } from "../../components/IndicatorSumarized";
 import mainImage from "../../assets/mainImage.svg";
+import { indicators } from "../../constants";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
+import { saveAs } from "file-saver";
+import ExcelJS from "exceljs";
 
 function Home() {
   const [answers, setAnswers] = useState([]);
@@ -52,6 +56,14 @@ function Home() {
         const data = await response.json();
         const answers = data?.data;
         answersSumarized[index].answers = answers;
+
+        //adding number of indicator from const indicator
+        indicators.forEach((indicator) => {
+          if (indicator.idIndicator === answersSumarized[index].indicator_id) {
+            answersSumarized[index].name = indicator.name;
+            console.log("indicator", indicator);
+          }
+        });
       }
       //order desc by id
       answersSumarized.sort((a, b) => b.id - a.id);
@@ -64,9 +76,72 @@ function Home() {
       setLoading(false);
     }
   };
-  // useEffect(() => {
-  //     getAnswers();
-  // }, []);
+  //function to create Excel file with answersSumarized when click on button
+
+  const createExcelFile = () => {
+    console.log("createExcelFile");
+    //create a new Excel file
+    const excel = new ExcelJS.Workbook();
+    //add a sheet to the Excel file
+    const sheet = excel.addWorksheet("Capturas");
+    //add the headers
+    sheet.addRow([
+      "id",
+      "Indicador",
+      "Nombre",
+      "Fecha captura",
+      "Nombre",
+      "Respuestas",
+    ]);
+    //add the data
+    answersDetailed.forEach((answerDetailed) => {
+      sheet.addRow([
+        answerDetailed.id,
+        answerDetailed.indicator_id,
+        answerDetailed.name,
+        new Date(answerDetailed.created_at).toLocaleDateString("es-MX"),
+        `${answerDetailed.name_user} 
+        ${answerDetailed.last_name_user_1}
+        ${answerDetailed.last_name_user_2} `,
+        answerDetailed.answers.length,
+      ]);
+    });
+    // in a new sheet save the answers
+    const sheetAnswers = excel.addWorksheet("Respuestas");
+    //add the headers
+    sheetAnswers.addRow([
+      "id",
+      "device_id",
+      "indicator_id",
+      "question_id",
+      "answer_option",
+      "answer_value",
+    ]);
+    //add the data
+    answersDetailed.forEach((answerDetailed) => {
+      answerDetailed.answers.forEach((answer) => {
+        sheetAnswers.addRow([
+          answer.id,
+          answer.device_id,
+          answer.indicator_id,
+          answer.question_id,
+          answer.answer_option,
+          answer.answer_value,
+        ]);
+      });
+    });
+
+    //save the Excel file
+    excel.xlsx
+      .writeBuffer()
+      .then((buffer) => {
+        const blob = new Blob([buffer], { type: "application/octet-stream" });
+        saveAs(blob, "Capturas.xlsx");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   return (
     <div>
@@ -94,36 +169,58 @@ function Home() {
           {errorLoading && <p>Error al obtener la información</p>}
         </div>
       </div>
-      <table className="table-auto w-full mb-10">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="px-4 py-2">Número</th>
-            <th className="px-4 py-2">Indicador</th>
-            <th className="px-4 py-2">Usuario</th>
-            <th className="px-4 py-2">Respuestas</th>
-          </tr>
-        </thead>
-        <tbody>
-          {answersDetailed.map((answerDetailed) => (
-            <tr key={answerDetailed.id}>
-              <td className="border px-4 py-2 text-center">
-                {answerDetailed.id}
-              </td>
-              <td className="border px-4 py-2 text-center">
-                {answerDetailed.indicator_id}
-              </td>
-              <td className="border px-4 py-2 text-center">
-                {`${answerDetailed.name_user} 
-                ${answerDetailed.last_name_user_1}
-                ${answerDetailed.last_name_user_2} `}
-              </td>
-              <td className="border px-4 py-2 text-center">
-                {answerDetailed.answers.length}
-              </td>
+      {answersDetailed.length > 0 && (
+        <div className="flex flex-row-reverse mr-16  ">
+          <button className=" flex " onClick={() => createExcelFile()}>
+            <ArrowDownTrayIcon className="w-6 h-6" />
+          </button>
+        </div>
+      )}
+      <div className="flex flex-col justify-center">
+        <table className="table-auto mb-10 m-10">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-4 py-2">id</th>
+              <th className="px-4 py-2">Indicador</th>
+              <th className="px-4 py-2">Nombre</th>
+              <th className="px-4 py-2">Fecha captura</th>
+              <th className="px-4 py-2">Nombre</th>
+              <th className="px-4 py-2">Respuestas</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {answersDetailed.map((answerDetailed) => (
+              <tr key={answerDetailed.id}>
+                <td className="border px-4 py-2 text-center">
+                  {answerDetailed.id}
+                </td>
+                <td className="border px-4 py-2 text-center">
+                  {answerDetailed.indicator_id}
+                </td>
+                <td className="border px-4 py-2 text-left ">
+                  {answerDetailed.name}
+                </td>
+                <td className="border px-4 py-2 text-left ">
+                  {
+                    //format date day, month, year
+                    new Date(answerDetailed.created_at).toLocaleDateString(
+                      "es-MX"
+                    )
+                  }
+                </td>
+                <td className="border px-4 py-2 text-center">
+                  {`${answerDetailed.name_user} 
+                  ${answerDetailed.last_name_user_1}
+                  ${answerDetailed.last_name_user_2} `}
+                </td>
+                <td className="border px-4 py-2 text-center">
+                  {answerDetailed.answers.length}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
